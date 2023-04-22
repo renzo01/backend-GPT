030a6b369ead5bf563a53cf7131cfbf62ff7d6a6
= <=>
2023-04-21 23:11:07 -0500

    configuracion chatGPT

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+const { ChatGPTAPI } = require('chatgpt');
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 4000;
+
+//coneccion con chatGPT
+const gptApi = new ChatGPTAPI({
+    apiKey: process.env.OPENAI_API_KEY,
+    debug: true
+})
+
+app.post('/envio-correo-personalizado',(req, res) => {
+
+})
 
 app.get('/', (req,res) =>{
     res.send('!Hola');
