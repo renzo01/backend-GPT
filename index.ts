02de2a4e8279e5c8be309547f97bf44cf48b6726
= <=>
2023-04-22 09:04:41 -0500

    actulizacion de datos

---
+++ index.ts
@@ -0,0 +1,88 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import {
+  ChatCompletionRequestMessage,
+  CreateChatCompletionRequest,
+  Configuration,
+  OpenAIApi,
+} from "openai";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import GPT3TokenizerImport from "gpt3-tokenizer";
+
+const GPT3Tokenizer: typeof GPT3TokenizerImport =
+  typeof GPT3TokenizerImport === "function"
+    ? GPT3TokenizerImport
+    : (GPT3TokenizerImport as any).default;
+
+const tokenizer = new GPT3Tokenizer({ type: "gpt3" });
+
+function getTokens(input: string): number {
+  const tokens = tokenizer.encode(input);
+  return tokens.text.length;
+}
+
+dotenv.config();
+
+const port = 4000;
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.post('/chat',async (req:Request,res: Response) => {
+    const requestMessages: ChatCompletionRequestMessage[] = req.body.messages;
+
+    try {
+      let tokenCount = 0;
+        console.log(requestMessages);
+      requestMessages.forEach((msg) => {
+        const tokens = getTokens(msg.content);
+        tokenCount += tokens;
+      });
+  
+      const moderationResponse = await openai.createModeration({
+        input: requestMessages[requestMessages.length - 1].content,
+      });
+      if (moderationResponse.data.results[0]?.flagged) {
+        return res.status(400).send("Message is inappropriate");
+      }
+  
+      const prompt =
+        'Eres NTTDoctor, un experto en medicina, tu labor es ayudar las personas a identificar sus enfermedades y derivarlo a la mejor especialidad para su caso';
+  
+      tokenCount += getTokens(prompt);
+      if (tokenCount > 4000) {
+        return res.status(400).send("Message is too long");
+      }
+  
+      const apiRequestBody: CreateChatCompletionRequest = {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "system", content: prompt }, ...requestMessages],
+        temperature: 0.6,
+      };
+      const completion = await openai.createChatCompletion(apiRequestBody);
+  
+      res.json(completion.data);
+    } catch (error) {
+      if (error instanceof Error) {
+        // @ts-ignore
+        console.log(error);
+      }
+      res.status(500).send("Something went wrong");
+    }
+})
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
